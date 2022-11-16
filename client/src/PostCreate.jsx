06ad149b669ba4bdc142406b1a3f171130f474e3
Rawanul Momen <Rusaf@Rawanuls-MacBook-Pro.local>
2022-11-15 21:34:32 -0500

    Minor formatting

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import PostList from './PostList';
 
 const PostCreate = () => {
+  const [title, setTitle] = useState('');
 
-    const [title, setTitle] = useState('');
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
+    await axios.post('http://posts.com/posts/create', {
+      title: title,
+    });
 
-        await axios.post('http://posts.com/posts/create', {
-            title: title
-        });
+    setTitle('');
+  };
 
-        setTitle('');
-    }
-
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <label>Title</label>
-                    <input
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className='form-control'
-                    /> 
-                </div>
-                <button className='btn btn-primary'>Submit</button>
-            </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='form-control'
+          />
         </div>
-    )
-}
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default PostCreate
+export default PostCreate;
